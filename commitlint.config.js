5fa6026a252d8b90f2541ced25928083d307642e
Kimura <dnanf12345@gmail.com>
2026-01-05 17:23:56 +0900

    init: apps 제외 설정완료

---
+++ commitlint.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'init',
+        'feat',
+        'fix',
+        'docs',
+        'refactor',
+        'chore',
+        'design',
+        'hotfix',
+        'test',
+        'wip',
+      ],
+    ],
+    'subject-case': [
+      2,
+      'never',
+      ['sentence-case', 'start-case', 'pascal-case', 'upper-case'],
+    ],
+    'subject-full-stop': [2, 'never', '.'],
+    'header-max-length': [2, 'always', 50],
+  },
+};
