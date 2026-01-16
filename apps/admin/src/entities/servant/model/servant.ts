1c7d90e2b5992d06770c87ac92c77550579a7d83
Kimura <dnanf12345@gmail.com>
2026-01-16 16:31:12 +0900

    wip: servant

---
+++ apps/admin/src/entities/servant/model/servant.ts
@@ -0,0 +1,11 @@
+export interface Servant {
+  id: string;
+  name: string;
+  role: string;
+  photoUrl: string;
+  contact: string;
+  introduction: string;
+  isPublic: boolean;
+  sortOrder: number;
+  createdAt: string;
+}
